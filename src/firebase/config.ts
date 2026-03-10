7cfa85510448e6e7de57904cbb5b41e2c36b306e
Lucas Souza <lucassouza.sou@gmail.com>
2026-03-10 16:29:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-347318012-d5b23",
+  "appId": "1:370895658315:web:4a63e17a0b77197d01dcce",
+  "apiKey": "AIzaSyAi0vo2d6O8_mnJ3NdMTqnemvAU7VZ3HyY",
+  "authDomain": "studio-347318012-d5b23.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "370895658315"
+};
